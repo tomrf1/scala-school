b774fddd23724942217d3e29be43d26d2547de73
Tom Forbes <tom.forbes@theguardian.com>
2021-08-05 13:44:24 +0100

    start adding a capi service

--- webserver-project/app/wiring/AppComponents.scala
+++ webserver-project/app/wiring/AppComponents.scala
@@ -4,9 +4,10 @@ import play.api.routing.Router
 import play.api.ApplicationLoader.Context
 import play.api.{BuiltInComponentsFromContext, NoHttpFiltersComponents}
 import controllers._
+import play.api.libs.ws.ahc.AhcWSComponents
 import router.Routes
 
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with NoHttpFiltersComponents with AssetsComponents {
+class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with NoHttpFiltersComponents with AssetsComponents with AhcWSComponents {
   override lazy val router: Router = new Routes(
     httpErrorHandler,
     new HelloWorldController(controllerComponents)

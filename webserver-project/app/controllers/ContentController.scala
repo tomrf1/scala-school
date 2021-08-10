9ca54dc361de49f66d3d176bce761eaf71627c8d
Tom Forbes <tom.forbes@theguardian.com>
2021-08-10 19:55:25 +0100

    errors

--- webserver-project/app/controllers/ContentController.scala
+++ webserver-project/app/controllers/ContentController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import play.api.mvc.{AbstractController, ControllerComponents}
-import services.CapiService
+import services.{CapiNotFound, CapiServerError, CapiService}
 
 import scala.concurrent.ExecutionContext
 
@@ -9,7 +9,11 @@ class ContentController(components: ControllerComponents, capiService: CapiServi
   def content(path: String) = Action.async {
     capiService
       .fetchContent(path)
-      .map(content => Ok(content))
+      .map {
+        case Right(content) => Ok(content)
+        case Left(CapiNotFound()) => NotFound(s"Not found: $path")
+        case Left(CapiServerError(message)) => InternalServerError(s"Capi says: $message")
+      }
       .recover { case error =>
         println(error)
         InternalServerError(error.getMessage)
--- webserver-project/app/services/CapiService.scala
+++ webserver-project/app/services/CapiService.scala
@@ -4,13 +4,21 @@ import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
+sealed trait CapiError
+case class CapiNotFound() extends CapiError
+case class CapiServerError(message: String) extends CapiError
+
 class CapiService(wsClient: WSClient)(implicit ec: ExecutionContext) {
   val url = "https://content.guardianapis.com"
   val apiKey = "test"
 
-  def fetchContent(path: String): Future[String] =
+  def fetchContent(path: String): Future[Either[CapiError, String]] =
     wsClient
       .url(s"$url/$path?api-key=$apiKey")
       .get()
-      .map(response => response.body)
+      .map(response => response.status match {
+        case 200 => Right(response.body)
+        case 404 => Left(CapiNotFound())
+        case _ => Left(CapiServerError(response.statusText))
+      })
 }

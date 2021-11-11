c4f126225e9662d1f74ed06e8bf4860d9a340029
Tom Forbes <tom.forbes@theguardian.com>
2021-11-11 08:44:50 +0000

    no comment

--- corporate-connections/app/services/CompaniesHouseService.scala
+++ corporate-connections/app/services/CompaniesHouseService.scala
@@ -5,13 +5,7 @@ import play.api.libs.ws.{WSAuthScheme, WSClient}
 import scala.concurrent.{ExecutionContext, Future}
 
 class CompaniesHouseService(wsClient: WSClient, apiKey: String)(implicit ec: ExecutionContext) {
-  /**
-   * E.g.
-   * curl -XGET -u <api-key>: https://api.company-information.service.gov.uk/officers/aEdQfmEjiBuB7tLwOP_Wfg-JA-8/appointments
-   *
-   * curl -XGET -u <api-key>: https://api.company-information.service.gov.uk/company/03114488/officers
-   */
-
+  
   // TODO - what should this return?
   def getCompanies(officerId: String): Unit = {
     // TODO - get list of companies linked to officer
